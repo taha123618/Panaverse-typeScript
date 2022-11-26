11566eca310029bc8438af4fd1718c3bfe64e544
taha123618 <tahaahmedanees2@gmail.com>
2022-11-26 23:22:00 +0500

    Adding Calculator in typescript

---
+++ Claculator/Answers/index.js
@@ -0,0 +1,1 @@
+export const answers = console.log("ok");
---
+++ Claculator/index.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+import { questions } from "./Questions/index.js";
+import { answers } from "./Answers/index.js";
+// const calculator: any = await inquirer.prompt([
+//   {
+//     name: "no",
+//     type: "number",
+//     message: "Enter your No:",
+//   },
+//   {
+//     name: "op",
+//     type: "checkbox",
+//     message: "Choose Operator",
+//   },
+//   {
+//     name: "con",
+//     type: "confirm",
+//   },
+// ]);
+// if (questions.no >= 0) {
+//   console.log(chalkAnimation.rainbow("Your No" + questions.no));
+// } else {
+//   console.log(chalk.red("Error on Logic"));
+// }
+const data = () => {
+    inquirer.prompt(questions).then(answers);
+};
+data();
